fd7d15532867589e8d16742ee0b198584e68c184
sharadpokale11@gmail.com <sharadpokale11@gmail.com>
2020-11-01 20:16:45 +0530

    again

--- src/test/java/Udeymy/MavenJava/RESTAPITest.java
+++ src/test/java/Udeymy/MavenJava/RESTAPITest.java
@@ -13,6 +13,7 @@ System.out.println("Post Jira");
 System.out.println("I am doing chinges");
 System.out.println("This is from the second person");
 System.out.println("Doing some changes from first persion");
+System.out.println("Changes made by second again");
 }
 @Test
 public void deleteTwitter()
