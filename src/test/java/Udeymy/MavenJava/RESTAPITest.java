26dbcd173c394035cad373ae8c5c28cce0dcb743
sharadpokale11@gmail.com <sharadpokale11@gmail.com>
2020-11-01 22:13:45 +0530

    adding again

--- src/test/java/Udeymy/MavenJava/RESTAPITest.java
+++ src/test/java/Udeymy/MavenJava/RESTAPITest.java
@@ -8,26 +8,17 @@ public class RESTAPITest
 public void postJira()
 {
 System.out.println("Post Jira");	
-System.out.println("Post Jira");
-System.out.println("Post Jira");
-System.out.println("I am doing chinges");
-System.out.println("This is from the second person");
-System.out.println("Doing some changes from first persion");
-System.out.println("Changes made by second again");
+
 }
 @Test
 public void deleteTwitter()
 {
 System.out.println("Delete Twitter");
-System.out.println("Delete Twitter");
-System.out.println("Delete Twitter");System.out.println("Delete Twitter");
 
 }
 @Test
 public void deleteTwitter2()
 {
-System.out.println("Delete Twitter2");
-System.out.println("Delete Twitter2");
 System.out.println("Delete Twitter2");
 
 }
