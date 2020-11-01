d15190afbc3d23eefa778e6fd4354ab6fdbfbb32
sharadpok <sharadpokale11@gmail.com>
2020-11-01 20:10:17 +0530

    Now understand

--- src/test/java/Udeymy/MavenJava/RESTAPITest.java
+++ src/test/java/Udeymy/MavenJava/RESTAPITest.java
@@ -12,6 +12,7 @@ System.out.println("Post Jira");
 System.out.println("Post Jira");
 System.out.println("I am doing chinges");
 System.out.println("This is from the second person");
+System.out.println("Doing some changes from first persion");
 }
 @Test
 public void deleteTwitter()
